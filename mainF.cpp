399627c9b581f20fe7494a5373b5adb283a48614
Liana <liana.mkhitaryan37@gmail.com>
2019-03-23 20:05:32 +0400

    Add Skin Detection

--- mainF.cpp
+++ mainF.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 Mat bg, bgU;
 void run_avg(const Mat& img_gray, double avg);
-bool segment(const Mat & img_gray, vector<vector<Point> > & out_segment, Mat & out_thresholded, size_t treshold = 25);
+bool segment(const Mat & img_gray, vector<vector<Point> > & out_segment, Mat & out_thresholded, int& index_out,double treshold = 25);
+void getSkin(const Mat& input, Mat &skin);
 
 int main()
 {
@@ -27,7 +28,7 @@ int main()
 	namedWindow("Gray_image", WINDOW_AUTOSIZE);
 	namedWindow("Thresholded_image", WINDOW_AUTOSIZE);
 	namedWindow("ROI", WINDOW_AUTOSIZE);
-	char a[40];
+
 	int count = 0;
 	size_t num_frames = 0;
 	while (1) {
@@ -38,16 +39,18 @@ int main()
 		}
 		Rect roi(340, 100, 270, 270);
 		img_roi = img(roi);
-		cvtColor(img_roi, img_gray, COLOR_BGR2GRAY);
 
+		getSkin(img_roi, img_gray);
 
 		GaussianBlur(img_gray, img_gray, Size(7, 7), 0.0, 0);
+
+		
 		if (num_frames < 30)
 			run_avg(img_gray, aWeight);
 		else {
-
-			if (segment(img_gray, img_segment, img_threshold)) {
-				drawContours(img, img_segment, -1, Scalar(0, 255, 0), 1, 8, noArray(), 2000, Point(340, 100));
+			int index;
+			if (segment(img_gray, img_segment, img_threshold,index,30)) {
+				drawContours(img, img_segment, index, Scalar(0, 255, 0), 1, 8, noArray(), 2147483647, Point(340, 100));
 			}
 		}
 		rectangle(img, roi, Scalar(200, 255, 0));
@@ -63,12 +66,15 @@ int main()
 		}
 
 	}
-
-
-
 	return 0;
 }
+void getSkin(const Mat& input,Mat &skin)
+{
+	cvtColor(input, skin, COLOR_BGR2HSV);
+	//HSV
+	inRange(skin, Scalar(0, 30, 60), Scalar(20, 150, 255), skin);
 
+};
 void run_avg(const Mat & img_gray, double avg)
 {
 	if (bg.empty()) {
@@ -77,28 +83,44 @@ void run_avg(const Mat & img_gray, double avg)
 	}
 	accumulateWeighted(img_gray, bg, avg);
 }
+void removeBackground(const Mat& img, const Mat& bg, Mat & out, double thresholdOffset) {
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			uchar framePixel = img.at<uchar>(i, j);
+			uchar bgPixel = bg.at<uchar>(i, j);
 
-bool segment(const Mat & img_gray, vector<vector<Point> > & out_segment, Mat & out_thresholded, size_t treshold)
+			if (framePixel >= bgPixel - thresholdOffset && framePixel <= bgPixel + thresholdOffset)
+				out.at<uchar>(i, j) = 0;
+			else
+				out.at<uchar>(i, j) = 255;
+		}
+	}
+}
+bool segment(const Mat & img_gray, vector<vector<Point> > & out_segment, Mat & out_thresholded, int & index_out,double treshold)
 {
-	Mat diff;
+	Mat diff(Size(img_gray.rows,img_gray.cols), CV_8UC1);
 	if (bgU.empty())
 		bg.convertTo(bgU, CV_8UC1);
+	//Opencv versions
+	//absdiff(img_gray, bgU, diff);
 
-	absdiff(img_gray, bgU, diff);
+	//threshold(diff, out_thresholded, treshold, 255, THRESH_BINARY);
 
-
-	threshold(diff, out_thresholded, treshold, 255, THRESH_BINARY);
-
+	removeBackground(img_gray, bgU, diff, treshold);
+	out_thresholded = diff;
 	findContours(out_thresholded, out_segment, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	if (out_segment.empty())
 		return false;
-	int indexOfBiggestContour = -1;
-	size_t sizeOfBiggestContour = 0;
+	index_out = -1;
+	double sizeOfBiggestContour = 0;
 
-	for (size_t i = 0; i < out_segment.size(); i++)
-		if (out_segment[i].size() > sizeOfBiggestContour) {
-			sizeOfBiggestContour = out_segment[i].size();
-			indexOfBiggestContour = i;
+	for (int i = 0; i < out_segment.size(); i++) {
+		double x = contourArea(out_segment[i]);
+		if (x > sizeOfBiggestContour) {
+			sizeOfBiggestContour = x;
+			index_out = i;
 		}
-
+	}
+		
+	return true;
 }
